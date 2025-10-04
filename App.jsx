0d1f4def933815b8a942ec7b0becade44ab5d806
Sanskriti <Sanskriti.24bcs10247@sst.scaler.com>
2025-10-04 09:52:05 +0530

    Smooth scrolling for desktops and touchScreens

--- App.jsx
+++ App.jsx
@@ -18,7 +18,8 @@ function Home() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
+      smoothWheel: true,
+      smoothTouch: true,
     });
 
     // Animation loop
